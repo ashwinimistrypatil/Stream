a66a4852fbded857c681d4980372fe299dfd3dba
ashwinimistrypatil <ashwinimistrypatil@gmail.com>
2026-03-26 12:51:54 +0530

    Update even.java
    commiting sysout

--- StreamPrograms/src/stream/even.java
+++ StreamPrograms/src/stream/even.java
@@ -7,7 +7,7 @@ import java.util.stream.Collector;
 public class even {
 		public static void main(String args[]) {
 			List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
-			System.out.println(numbers);
+			//System.out.println(numbers);
 			numbers.stream()
 			       .filter(n -> n % 2 == 0)   
 			       .forEach(System.out::println);
